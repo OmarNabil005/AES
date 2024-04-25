c41c4d31df9fed26d3c95a95175e92a257a1330e
Omar Nabil <139366121+OmarNabil005@users.noreply.github.com>
2024-04-25 13:28:22 +0200

    BCD -> 7seg implementation

---
+++ BCD_to_7seg.v
@@ -0,0 +1,20 @@
+module BCD_to_7seg(input[3:0] in, output reg [6:0] out);
+      
+    always
+    begin
+        case (in)
+            0 : out = 7'b1000000;
+            1 : out = 7'b1111001;
+            2 : out = 7'b0100100;
+            3 : out = 7'b0110000;
+            4 : out = 7'b0011001;
+            5 : out = 7'b0010010;
+            6 : out = 7'b0000010;
+            7 : out = 7'b1111000;
+            8 : out = 7'b0000000;
+            9 : out = 7'b0010000;
+            default : out = 7'b1111111; 
+        endcase
+    end
+    
+endmodule
