3f7d24e8a68d05b7257da57ad967db6004691f5c
OmarGamal10 <ogamal1011@gmail.com>
2024-04-29 23:57:28 +0300

    :wrench: Slight fix for Quartus synthesize

--- KeyExpansion128bit.v
+++ KeyExpansion128bit.v
@@ -7,7 +7,7 @@ assign keyschedule[0:127]=key;
 genvar i;
 
 generate
-    for(i=4;i<44;i=i+1)begin //generate word by word from (4) to (43)
+    for(i=4;i<44;i=i+1)begin : GenerateBlock //generate word by word from (4) to (43)
         if(i%4==0)begin
         assign keyschedule[(i * 32) +: 32]=Rotate(subword(keyschedule[(i - 1)* 32 +: 32]))^ keyschedule[( i - 4) * 32 +: 32] ^Rcon(i/4);
         end
