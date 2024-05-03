b308f53ea99cf6dc5352d736b52cd03b5f7ccff8
Omar Nabil <139366121+OmarNabil005@users.noreply.github.com>
2024-05-04 01:53:21 +0300

    optimized keyExp wires & fixed AES module for different bit sizes

--- AES.v
+++ AES.v
@@ -1,26 +1,30 @@
-module AES #(parameter nk=4, parameter nr=10)(input [0:127] Message, input [0:127] Key, input clk, output [6:0] HEX2, output [6:0] HEX1, output [6:0] HEX0);
+module AES #(parameter nk=4, parameter nr=10)(Message, Key, clk, HEX2, HEX1, HEX0);
 
-//wire [7:0] o2, o1, o;
+input clk;
+input [0:127] Message;
+input [0:(32 * nk) - 1] Key;
+output [6:0] HEX2;
+output [6:0] HEX1;
+output [6:0] HEX0;
 
 reg [0:127] stateReg;
 wire [0: 128 * (nr + 1) -1] KeySchedule;
 
 initial begin
-    stateReg = 128'h0;
-end    
-KeyExpansion128 keyExp(Key, KeySchedule);
-
+    stateReg = 0;
+end
 reg [0:4] i = 5'b00000;
-
-wire [0:127] out0;
-AddRoundKey initialRound(Message,KeySchedule[0:127],out0);
-
-Binary_to_7seg hexa(stateReg[120:127], HEX2, HEX1, HEX0);
-
-wire [0:127] out1, out2, out3, out4, out5,
-             out6, out7, out8, out1Final,
+wire [0:127] out0, out1, out2, out3, out4,
+             out5,out6, out7, out8, out1Final,
              out2Final, out3Final, out4Final,
              out5Final, out6Final, try;
+
+KeyExpansion #(nk, nr) keyExp(Key, KeySchedule);
+
+AddRoundKey initialRound(Message,KeySchedule[0:127],out0);
+
+Binary_to_7seg hexa(stateReg[120 +: 8], HEX2, HEX1, HEX0);
+
 
 SubBytes subbytes(stateReg,out1);
 ShiftRows shiftrows(out1,out2);
@@ -30,34 +34,34 @@ AddRoundKey addround(out3,KeySchedule[128*i +: 128],out4);
 always @(posedge clk) 
 begin
     if(i<1)begin
-    i<=i+4'b0001;
+    i <= i + 1;
     stateReg=out0;
     end
     else if(i<nr)begin
-    i<=i+4'b0001;
+    i <= i + 1;
     stateReg=out4;
     end
     else if (i == nr)begin
-    i<=i+4'b0001;
+    i <= i + 1;
     stateReg=out3Final;
     end
     
 
     // Inv Cipher !!!
     else if(i==(nr + 1))begin
-        i<= i+4'b0001;
+        i <= i + 1;
         stateReg=try;
     end
-    else if(i<(nr + 11))begin
-    i<=i+4'b0001;
+    else if(i<=(2 * nr))begin
+    i <= i + 1;
     stateReg=out8;
     end
-    else if (i == (nr + 11))begin
-    i<=i+4'b0001;
+    else if (i == (2 * nr + 1))begin
+    i <= i + 1;
     stateReg=out6Final;
     end
 end
-assign cipher=stateReg;
+//assign cipher=stateReg;
 
 SubBytes subbytess(stateReg,out1Final);
 ShiftRows shift(out1Final,out2Final);
@@ -70,7 +74,7 @@ AddRoundKey inv_initialll (stateReg,KeySchedule[(128 * nr) +:128],try);
 
 inv_SubBytes inv_subbytes(stateReg,out5);
 invShiftRows inv_shiftrows(out5,out6);
-AddRoundKey inv_addround(out6,KeySchedule[(128 * nr)-128*(i - (nr + 1)) +:128],out7);
+AddRoundKey inv_addround(out6,KeySchedule[(128 * (2 * nr - i + 1)) +:128],out7);
 inverseMixColumns inv_mixcol(out7,out8);
 
 inv_SubBytes inv1_subbytess(stateReg,out4Final);
@@ -86,7 +90,7 @@ module testbench1();
 
 // Parameters
 wire [127:0] message =128'h00112233445566778899aabbccddeeff; // Fixed message
-wire [127:0] key = 128'h000102030405060708090a0b0c0d0e0f ; // Fixed key
+wire [255:0] key = 256'h603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4; // Fixed key
 // Signals
 wire [6:0] HEx2;
 wire [6:0] HEx1;
@@ -98,7 +102,7 @@ reg clk;
 
 
 // Instantiate the module under test
-AES #(4, 10) encrypt_inst (
+AES #(8, 14) encrypt_inst (
     .Message(message),
     .Key(key),
     .clk(clk),
--- KeyExpansion.v
+++ KeyExpansion.v
@@ -4,32 +4,32 @@ module KeyExpansion #(parameter nk=4, parameter nr=10)(key, keyschedule);
 input [0:((32 * nk) - 1)] key;
 output [0:(128 * (nr + 1)) - 1] keyschedule;
 
-assign keyschedule[0:(nk * 32) - 1]=key;
+assign keyschedule[0:(nk * 32) - 1] = key;
 
 
-wire [0:(128 * (nr + 1)) - (32 * nk) - 1] sWord;
-wire [0:(128 * (nr + 1)) - (32 * nk) - 1] sWord256;
+wire [0:(32 * ((4 * nr) / nk)) - 1] sWord;
+wire [0:191] sWord256;
 
  genvar i;
     generate
+    if(nk < 8)
+        assign sWord256 = 192'h0;
     for (i=nk;i< 4 * (nr + 1);i=i+1)begin: GenerateBlockS
-	if (i > 0)begin
-	subword swa(rotword(keyschedule[((i - 1) * 32) +: 32]), sWord[((i - nk) * 32) +: 32]);
-	subword sw256(keyschedule[(i - 1)* 32 +: 32], sWord256[((i - nk) * 32) +: 32]);
-	end
+
         if(i%nk==0)begin
-       assign keyschedule[(i * 32) +: 32]=  keyschedule[((i - nk) * 32) +: 32]  ^ sWord[((i - nk) * 32) +: 32] ^ rconx(i/nk);
+	    subword swa(rotword(keyschedule[((i - 1) * 32) +: 32]), sWord[(((i / nk) - 1) * 32) +: 32]);
+        assign keyschedule[(i * 32) +: 32]=  keyschedule[((i - nk) * 32) +: 32]  ^ sWord[(((i / nk) - 1) * 32) +: 32] ^ rconx(i/nk);
         end
 		else if (nk > 6 && i % 8 == 4)begin
-        assign keyschedule[(i * 32) +: 32]= sWord256[((i - nk) * 32) +: 32] ^ keyschedule[( i - 8) * 32 +: 32];
-        end
-        else begin
+    	subword sw256(keyschedule[(i - 1)* 32 +: 32], sWord256[(((i - 12) / 8) * 32) +: 32]);
+        assign keyschedule[(i * 32) +: 32]= sWord256[(((i - 12) / 8) * 32) +: 32] ^ keyschedule[( i - 8) * 32 +: 32];
+        end        
+        else 
         assign keyschedule[((i)*32) +: 32]=  keyschedule[((i - 1) * 32) +: 32] ^ keyschedule[ ((i-nk)*32) +: 32];
-        end
+        
     end
 
 endgenerate
-
 
 
 function[0:31] rconx;
@@ -70,25 +70,21 @@ endfunction
 endmodule
 
 module KeyExpansion128_tbgemy();
-	reg [0:127] keyin;
-	wire [0:(128*11) - 1] keys;
+	reg [0:255] keyin;
+	wire [0:(128*15) - 1] keys;
 
-	KeyExpansion #(4,10) uut(keyin,keys);
+	KeyExpansion #(8,14) uut(keyin,keys);
 
 	integer i;
 
 	initial begin
-		keyin = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+		//keyin = 128'h2b7e151628aed2a6abf7158809cf4f3c;
 		//keyin = 192'h8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b;
-		//keyin = 256'h603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4;
+		keyin = 256'h603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4;
 		#10;
 
-		for (i = 0; i < 44; i = i + 1) begin
+		for (i = 0; i < 60; i = i + 1) begin
 			$display("keys[%0d] = %h", i, keys[(i * 32) +: 32]);
 		end
 	end
 endmodule
-
-
-
-
